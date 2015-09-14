60ae2d2e8ba358f83f1bd6afa9cf3eb740253bf8
Serge Barth <s.l.barth@uva.nl>
2015-09-14 13:28:38 +0200

    Basic version, calculates to decimal numbering system.

---
+++ RadixCalculator.py
@@ -0,0 +1,17 @@
+alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+input = "33"
+radix = 7.5
+
+input = input.upper( )
+res = 0
+for x in input:
+    d = alphabet.find( x )
+    if d < 0 or d > radix:
+        print "The given number is not in base ", radix
+        res = -1
+        break
+    else:
+        res = radix * res + d
+
+if res > -1:
+    print input, "in base", radix, "is", res, "in base 10."print input, "in base", radix, "is", res, "in base 10."
